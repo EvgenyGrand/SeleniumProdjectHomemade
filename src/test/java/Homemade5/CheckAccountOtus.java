3be22365aeba4bf85b3e2ebb07deca04a1c7ce0d
evgen <Evgeny.Grand@mail.ru>
2022-10-12 21:33:25 +0300

    Homework 5 qes

--- src/test/java/Homemade5/CheckAccountOtus.java
+++ src/test/java/Homemade5/CheckAccountOtus.java
@@ -1,22 +1,27 @@
 package Homemade5;
 
 import homemade4.ThirdTask;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import utils.BaseTestWindow;
 
+import java.util.concurrent.TimeUnit;
+
 public class CheckAccountOtus extends BaseTestWindow {
 
 
     private final String LOGIN = "Evgeny.Grand@mail.ru";
     private final String PASSWORD = "Lufthansa12";
-
 
 
     @Test
@@ -53,9 +58,9 @@ public class CheckAccountOtus extends BaseTestWindow {
         fname.clear();
         fname.sendKeys("Евгений");
         //Фамилия
-        WebElement lname= driver.findElement(By.id("id_lname"));
-        fname.clear();
-        fname.sendKeys("Чистяков");
+        WebElement lname = driver.findElement(By.id("id_lname"));
+        lname.clear();
+        lname.sendKeys("Чистяков");
         //Имя Латиницей
         WebElement nameLatin = driver.findElement(By.id("id_fname_latin"));
         nameLatin.clear();
@@ -66,12 +71,12 @@ public class CheckAccountOtus extends BaseTestWindow {
         lNAmeLatin.sendKeys("Chistyakov");
         //Псевдоним
         WebElement id_blog_name = driver.findElement(By.id("id_blog_name"));
-        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", id_blog_name);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", id_blog_name);
         id_blog_name.clear();
         id_blog_name.sendKeys("Eвгений");
         //День рождения
         WebElement dayOfBirth = driver.findElement(By.cssSelector("input[title='День рождения']"));
-        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();",dayOfBirth);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", dayOfBirth);
         dayOfBirth.clear();
         dayOfBirth.sendKeys("20.06.1989");
         //Страна
@@ -89,22 +94,22 @@ public class CheckAccountOtus extends BaseTestWindow {
         //Уровень Английского
         WebElement englishLevel = driver.findElement(By.xpath("/html/body/div[1]/div/div[4]/div[3]/div[2]/div[2]/div/form/div[1]/div[3]/div[1]/div/div[1]/div[3]/div[2]/div/label/div"));
         englishLevel.click();
-        WebElement changeLevel=driver.findElement(By.cssSelector("button[title='Ниже среднего (Pre-Intermediate)']"));
+        WebElement changeLevel = driver.findElement(By.cssSelector("button[title='Ниже среднего (Pre-Intermediate)']"));
         changeLevel.click();
         //Добавление контакта 1
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("scroll(0,1500);");
         WebElement deleteContact = driver.findElement(By.cssSelector("body > div.body-wrapper > div > div.js-lk-cv > div.container.container-padding-bottom > div.container__row > div.container__col.container__col_9.container__col_md-8.container__col_sm-12.container__col_border-left.lk-rightbar.print-block.print-wide > div > form > div:nth-child(2) > div.container__col.container__col_12 > div:nth-child(2) > div.js-formset > div > div:nth-child(1) > div.container__col.container__col_3.container__col_md-12 > div > div.container__col.container__col_0.container__col_md-3.container__col_ssm-4 > div:nth-child(2) > button"));
-        WebElement contact1= driver.findElement(By.cssSelector("button[class='lk-cv-block__action lk-cv-block__action_md-no-spacing js-formset-add js-lk-cv-custom-select-add']"));
+        WebElement contact1 = driver.findElement(By.cssSelector("button[class='lk-cv-block__action lk-cv-block__action_md-no-spacing js-formset-add js-lk-cv-custom-select-add']"));
         contact1.click();
         Thread.sleep(2000);
-        WebElement changeContact1=driver.findElement(By.cssSelector("body > div.body-wrapper > div > div.js-lk-cv > div.container.container-padding-bottom > div.container__row > div.container__col.container__col_9.container__col_md-8.container__col_sm-12.container__col_border-left.lk-rightbar.print-block.print-wide > div > form > div:nth-child(2) > div.container__col.container__col_12 > div:nth-child(2) > div.js-formset > div > div:nth-child(3) > div.container__col.container__col_9.container__col_ssm-12 > div > div > div > label > div > span"));
+        WebElement changeContact1 = driver.findElement(By.cssSelector("body > div.body-wrapper > div > div.js-lk-cv > div.container.container-padding-bottom > div.container__row > div.container__col.container__col_9.container__col_md-8.container__col_sm-12.container__col_border-left.lk-rightbar.print-block.print-wide > div > form > div:nth-child(2) > div.container__col.container__col_12 > div:nth-child(2) > div.js-formset > div > div:nth-child(3) > div.container__col.container__col_9.container__col_ssm-12 > div > div > div > label > div > span"));
         changeContact1.click();
         Thread.sleep(2000);
-        WebElement whatssapcontact= driver.findElement(By.cssSelector("body > div.body-wrapper > div > div.js-lk-cv > div.container.container-padding-bottom > div.container__row > div.container__col.container__col_9.container__col_md-8.container__col_sm-12.container__col_border-left.lk-rightbar.print-block.print-wide > div > form > div:nth-child(2) > div.container__col.container__col_12 > div:nth-child(2) > div.js-formset > div > div:nth-child(3) > div.container__col.container__col_9.container__col_ssm-12 > div > div > div > div > div > button:nth-child(8)"));
+        WebElement whatssapcontact = driver.findElement(By.cssSelector("body > div.body-wrapper > div > div.js-lk-cv > div.container.container-padding-bottom > div.container__row > div.container__col.container__col_9.container__col_md-8.container__col_sm-12.container__col_border-left.lk-rightbar.print-block.print-wide > div > form > div:nth-child(2) > div.container__col.container__col_12 > div:nth-child(2) > div.js-formset > div > div:nth-child(3) > div.container__col.container__col_9.container__col_ssm-12 > div > div > div > div > div > button:nth-child(8)"));
         whatssapcontact.click();
         Thread.sleep(2000);
-        WebElement inputcontact= driver.findElement(By.cssSelector("input[name='contact-1-value']"));
+        WebElement inputcontact = driver.findElement(By.cssSelector("input[name='contact-1-value']"));
         inputcontact.sendKeys("+79218466331");
 
         // Пол
@@ -114,10 +119,49 @@ public class CheckAccountOtus extends BaseTestWindow {
         Thread.sleep(2000);
         changeGender.submit();
 
+        //закрытие браузера
+        driver.quit();
+
+        //Запуск чистого браузера
+        WebDriverManager.chromedriver().setup();
+        WebDriver admin = new ChromeDriver();
+        admin.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        logger.info("Драйвер поднят");
+
+        admin.manage().window().maximize();
+        admin.get("https://otus.ru");
+        Thread.sleep(2000);
+
+
+            //Выполнение входа в личный кабинет (в page object вынести в отдельный метод)
+            WebElement registrationButtonAdmin = admin.findElement(By.cssSelector("body > div.body-wrapper > div > header.header2.header2_non-auth > div > div.header2__right > div > div.header2__auth-container > button"));
+            registrationButtonAdmin.click();
+            Thread.sleep(2000);
+        WebElement loginAdmin = driver.findElement(By.cssSelector("input[type='text'][placeholder='Электронная почта']"));
+        WebElement passwordAdmin = driver.findElement(By.cssSelector("input[type='password']"));
+        WebElement buttonLKAdmin = driver.findElement(By.cssSelector("button[class='new-button new-button_full new-button_blue new-button_md']"));
+        Thread.sleep(2000);
+        loginAdmin.clear();
+        loginAdmin.sendKeys("Evgeny.Granf@mail.ru");
+        passwordAdmin.clear();
+        passwordAdmin.sendKeys(PASSWORD);
+        buttonLKAdmin.click();
+        logger.info("Вход в ЛК выполнен");
+        Thread.sleep(2000);
+
+        //Переход в Раздел ЛК "О себе"
+        Actions actionAdmin = new Actions(driver);
+        WebElement dropdownlistAdmin = driver.findElement(By.cssSelector("div[class = 'header2-menu__item-wrapper header2-menu__item-wrapper__username']"));
+        WebElement myAccountAdmin = driver.findElement(By.cssSelector("b[class='header2-menu__dropdown-text_name']"));
+        actionAdmin.moveToElement(dropdownlistAdmin).moveToElement(myAccountAdmin).click().build().perform();
+        logger.info("Переход в раздел 'О себе' выполнен");
 
 
 
 
+            //Проверки
 
+
+        }
     }
-}
+
--- src/test/java/utils/BaseTestWindow.java
+++ src/test/java/utils/BaseTestWindow.java
@@ -2,6 +2,7 @@ package utils;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -9,10 +10,13 @@ import java.util.concurrent.TimeUnit;
 public class BaseTestWindow extends BaseTestHeadless {
 
     @BeforeEach
-    public void setUpChrome(){
+    public void setUpChrome() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         logger.info("Драйвер поднят");
     }
-}
+
+
+
+    }
